8fd8cfa68d4102416922696303747d5419815269
SonHyo <h.son@skb.co.jp>
2014-08-29 11:01:59 +0900

    Ver1

---
+++ Calc/Form1.Designer.vb
@@ -0,0 +1,298 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Calculator
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Calculator))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button0 = New System.Windows.Forms.Button()
+        Me.ButtonDot = New System.Windows.Forms.Button()
+        Me.ButtonDiv = New System.Windows.Forms.Button()
+        Me.ButtonMulti = New System.Windows.Forms.Button()
+        Me.ButtonSub = New System.Windows.Forms.Button()
+        Me.ButtonAdd = New System.Windows.Forms.Button()
+        Me.ButtonEque = New System.Windows.Forms.Button()
+        Me.LabelDisplay = New System.Windows.Forms.Label()
+        Me.ButtonC = New System.Windows.Forms.Button()
+        Me.ButtonSymbol = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Location = New System.Drawing.Point(1, 198)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(50, 50)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "1"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Location = New System.Drawing.Point(50, 198)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(50, 50)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "2"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.Location = New System.Drawing.Point(99, 198)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(50, 50)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "3"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button4.Location = New System.Drawing.Point(1, 149)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(50, 50)
+        Me.Button4.TabIndex = 7
+        Me.Button4.Text = "4"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button5.Location = New System.Drawing.Point(50, 149)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(50, 50)
+        Me.Button5.TabIndex = 8
+        Me.Button5.Text = "5"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button6
+        '
+        Me.Button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button6.Location = New System.Drawing.Point(99, 149)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(50, 50)
+        Me.Button6.TabIndex = 9
+        Me.Button6.Text = "6"
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Button7
+        '
+        Me.Button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button7.Location = New System.Drawing.Point(1, 100)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(50, 50)
+        Me.Button7.TabIndex = 11
+        Me.Button7.Text = "7"
+        Me.Button7.UseVisualStyleBackColor = False
+        '
+        'Button8
+        '
+        Me.Button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button8.Location = New System.Drawing.Point(50, 100)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(50, 50)
+        Me.Button8.TabIndex = 12
+        Me.Button8.Text = "8"
+        Me.Button8.UseVisualStyleBackColor = False
+        '
+        'Button9
+        '
+        Me.Button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button9.Location = New System.Drawing.Point(99, 100)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(50, 50)
+        Me.Button9.TabIndex = 13
+        Me.Button9.Text = "9"
+        Me.Button9.UseVisualStyleBackColor = False
+        '
+        'Button0
+        '
+        Me.Button0.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button0.Location = New System.Drawing.Point(1, 247)
+        Me.Button0.Name = "Button0"
+        Me.Button0.Size = New System.Drawing.Size(99, 50)
+        Me.Button0.TabIndex = 1
+        Me.Button0.Text = "0"
+        Me.Button0.UseVisualStyleBackColor = False
+        '
+        'ButtonDot
+        '
+        Me.ButtonDot.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ButtonDot.Location = New System.Drawing.Point(99, 247)
+        Me.ButtonDot.Name = "ButtonDot"
+        Me.ButtonDot.Size = New System.Drawing.Size(50, 50)
+        Me.ButtonDot.TabIndex = 2
+        Me.ButtonDot.Text = "."
+        Me.ButtonDot.UseVisualStyleBackColor = False
+        '
+        'ButtonDiv
+        '
+        Me.ButtonDiv.BackColor = System.Drawing.Color.Orange
+        Me.ButtonDiv.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ButtonDiv.Location = New System.Drawing.Point(148, 51)
+        Me.ButtonDiv.Name = "ButtonDiv"
+        Me.ButtonDiv.Size = New System.Drawing.Size(50, 50)
+        Me.ButtonDiv.TabIndex = 17
+        Me.ButtonDiv.Text = "÷"
+        Me.ButtonDiv.UseVisualStyleBackColor = False
+        '
+        'ButtonMulti
+        '
+        Me.ButtonMulti.BackColor = System.Drawing.Color.Orange
+        Me.ButtonMulti.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ButtonMulti.Location = New System.Drawing.Point(148, 100)
+        Me.ButtonMulti.Name = "ButtonMulti"
+        Me.ButtonMulti.Size = New System.Drawing.Size(50, 50)
+        Me.ButtonMulti.TabIndex = 14
+        Me.ButtonMulti.Text = "×"
+        Me.ButtonMulti.UseVisualStyleBackColor = False
+        '
+        'ButtonSub
+        '
+        Me.ButtonSub.BackColor = System.Drawing.Color.Orange
+        Me.ButtonSub.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ButtonSub.Location = New System.Drawing.Point(148, 149)
+        Me.ButtonSub.Name = "ButtonSub"
+        Me.ButtonSub.Size = New System.Drawing.Size(50, 50)
+        Me.ButtonSub.TabIndex = 10
+        Me.ButtonSub.Text = "-"
+        Me.ButtonSub.UseVisualStyleBackColor = False
+        '
+        'ButtonAdd
+        '
+        Me.ButtonAdd.BackColor = System.Drawing.Color.Orange
+        Me.ButtonAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ButtonAdd.Location = New System.Drawing.Point(148, 198)
+        Me.ButtonAdd.Name = "ButtonAdd"
+        Me.ButtonAdd.Size = New System.Drawing.Size(50, 50)
+        Me.ButtonAdd.TabIndex = 6
+        Me.ButtonAdd.Text = "+"
+        Me.ButtonAdd.UseVisualStyleBackColor = False
+        '
+        'ButtonEque
+        '
+        Me.ButtonEque.BackColor = System.Drawing.Color.Orange
+        Me.ButtonEque.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ButtonEque.Location = New System.Drawing.Point(148, 247)
+        Me.ButtonEque.Name = "ButtonEque"
+        Me.ButtonEque.Size = New System.Drawing.Size(50, 50)
+        Me.ButtonEque.TabIndex = 0
+        Me.ButtonEque.Text = "="
+        Me.ButtonEque.UseVisualStyleBackColor = False
+        '
+        'LabelDisplay
+        '
+        Me.LabelDisplay.BackColor = System.Drawing.SystemColors.MenuText
+        Me.LabelDisplay.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.LabelDisplay.Font = New System.Drawing.Font("Digital-7", 30.0!)
+        Me.LabelDisplay.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.LabelDisplay.Location = New System.Drawing.Point(1, 1)
+        Me.LabelDisplay.Name = "LabelDisplay"
+        Me.LabelDisplay.Size = New System.Drawing.Size(197, 51)
+        Me.LabelDisplay.TabIndex = 17
+        Me.LabelDisplay.Text = "0"
+        Me.LabelDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'ButtonC
+        '
+        Me.ButtonC.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ButtonC.Location = New System.Drawing.Point(1, 51)
+        Me.ButtonC.Name = "ButtonC"
+        Me.ButtonC.Size = New System.Drawing.Size(50, 50)
+        Me.ButtonC.TabIndex = 15
+        Me.ButtonC.Text = "C"
+        Me.ButtonC.UseVisualStyleBackColor = False
+        '
+        'ButtonSymbol
+        '
+        Me.ButtonSymbol.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ButtonSymbol.Location = New System.Drawing.Point(50, 51)
+        Me.ButtonSymbol.Name = "ButtonSymbol"
+        Me.ButtonSymbol.Size = New System.Drawing.Size(50, 50)
+        Me.ButtonSymbol.TabIndex = 16
+        Me.ButtonSymbol.Text = "±"
+        Me.ButtonSymbol.UseVisualStyleBackColor = False
+        '
+        'Calculator
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(199, 298)
+        Me.Controls.Add(Me.ButtonSymbol)
+        Me.Controls.Add(Me.ButtonC)
+        Me.Controls.Add(Me.LabelDisplay)
+        Me.Controls.Add(Me.ButtonEque)
+        Me.Controls.Add(Me.ButtonAdd)
+        Me.Controls.Add(Me.ButtonSub)
+        Me.Controls.Add(Me.ButtonMulti)
+        Me.Controls.Add(Me.ButtonDiv)
+        Me.Controls.Add(Me.ButtonDot)
+        Me.Controls.Add(Me.Button0)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.KeyPreview = True
+        Me.Name = "Calculator"
+        Me.Text = "Calculator"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents Button8 As System.Windows.Forms.Button
+    Friend WithEvents Button9 As System.Windows.Forms.Button
+    Friend WithEvents Button0 As System.Windows.Forms.Button
+    Friend WithEvents ButtonDot As System.Windows.Forms.Button
+    Friend WithEvents ButtonDiv As System.Windows.Forms.Button
+    Friend WithEvents ButtonMulti As System.Windows.Forms.Button
+    Friend WithEvents ButtonSub As System.Windows.Forms.Button
+    Friend WithEvents ButtonAdd As System.Windows.Forms.Button
+    Friend WithEvents ButtonEque As System.Windows.Forms.Button
+    Friend WithEvents LabelDisplay As System.Windows.Forms.Label
+    Friend WithEvents ButtonC As System.Windows.Forms.Button
+    Friend WithEvents ButtonSymbol As System.Windows.Forms.Button
+
+End Class
---
+++ Calc/Form1.vb
@@ -0,0 +1,453 @@
+﻿Public Class Calculator
+
+    Dim numQueueList As New Queue() '計算対象
+    Dim calcModeQueueList As New Queue() '0:None 1:Add 2:Sub 3:Multi 4:Div
+    Dim operQueueList As New Queue() 'ボタン操作
+    Dim intDigit As Integer = 8 '計算桁の設定
+    Dim errLock As Boolean = False 'Errの時はTrue
+
+#Region "function"
+
+    '少数点を除いて、文字列長さの検測
+    Private Function GetLength(text As String) As Integer
+        Dim length As Integer = 0
+        If text.Contains(".") Then
+            length = text.Length - 1
+        Else
+            length = text.Length
+        End If
+        Return length
+    End Function
+
+    '計算対象キュー
+    Private Sub NumEnqueue(key As Double)
+        If numQueueList.Count >= 2 Then
+            numQueueList.Dequeue()
+        End If
+
+        numQueueList.Enqueue(key)
+    End Sub
+
+    '操作記録キュー
+    Private Sub OperEnqueue(key As String)
+        If operQueueList.Count >= 2 Then
+            operQueueList.Dequeue()
+        End If
+
+        operQueueList.Enqueue(key)
+    End Sub
+
+    '演算式キュー
+    Private Sub CalcModeEnqueue(key As Integer)
+        If calcModeQueueList.Count >= 2 Then
+            calcModeQueueList.Dequeue()
+        End If
+
+        calcModeQueueList.Enqueue(key)
+    End Sub
+
+#End Region
+
+#Region "Display"
+
+    'LCD画面で数字の入力
+    Private Sub Display_Show_Add(text As String)
+        If operQueueList.Count > 0 Then
+            If operQueueList.Peek = "Oper" Or operQueueList.Peek = "Eque" Then
+                LabelDisplay.Text = ""
+            End If
+        End If
+        '桁以上の場合は入力禁止
+        If GetLength(LabelDisplay.Text) >= intDigit Then
+            Return
+        End If
+
+        '押した数字を最末へ追加する
+        LabelDisplay.Text = LabelDisplay.Text + text
+        If LabelDisplay.Text.StartsWith("0") And Not LabelDisplay.Text.StartsWith("0.") Then
+            Dim realDisplay As Double = LabelDisplay.Text
+            LabelDisplay.Text = realDisplay
+        End If
+    End Sub
+
+    'LCD画面で小数点の入力
+    Private Sub Display_Show_Dot()
+        If operQueueList.Count > 0 Then
+            If operQueueList.Peek = "Oper" Or operQueueList.Peek = "Eque" Then
+                LabelDisplay.Text = "0"
+            End If
+        End If
+
+        '桁以上の場合は入力禁止
+        If GetLength(LabelDisplay.Text) >= intDigit Then
+            Return
+        End If
+
+        '小数点は既に存在しないことを判断する
+        If Not LabelDisplay.Text.Contains(".") Then
+            '小数点を最末へ追加する
+            LabelDisplay.Text = LabelDisplay.Text + "."
+        End If
+    End Sub
+
+    'LCD画面のクリア
+    Private Sub Display_Clear()
+        LabelDisplay.Text = "0"
+        numQueueList.Clear()
+        operQueueList.Clear()
+        calcModeQueueList.Clear()
+        errLock = False
+    End Sub
+
+#End Region
+
+#Region "Process"
+
+    '計算処理
+    Private Sub Process(oper As Integer)
+        Dim result As Double = 0.0
+
+        Dim tmpNum As Array = numQueueList.ToArray()
+
+        Select Case oper
+            Case 1 '+
+                result = tmpNum(0) + tmpNum(1)
+
+            Case 2 '-
+                result = tmpNum(0) - tmpNum(1)
+
+            Case 3 '×
+                result = tmpNum(0) * tmpNum(1)
+
+            Case 4 '÷
+                If tmpNum(1) = 0 Then
+                    LabelDisplay.Text = "Err"
+                    errLock = True
+                    Return
+                Else
+                    result = tmpNum(0) / tmpNum(1)
+                End If
+
+            Case Else
+                result = LabelDisplay.Text
+
+        End Select
+
+        '計算結果の桁を処理する
+        LabelDisplay.Text = ChangeDigit(result)
+
+        If Double.TryParse(LabelDisplay.Text, tmpNum(1)) Then
+            NumEnqueue(LabelDisplay.Text)
+        End If
+    End Sub
+
+    '計算結果の桁処理
+    Private Function ChangeDigit(input As Double) As String
+        Dim output As String = input
+
+        '有効桁内の場合
+        If GetLength(output) <= intDigit Then
+            Return input
+        End If
+
+        If output.Contains(".") Then
+            '有効桁外、小数の場合
+            output = output.Substring(0, intDigit + 1)
+            Dim tmp As Double = output
+            output = tmp
+        Else
+            '有効桁外、整数の場合
+            output = "Err"
+            errLock = True
+        End If
+
+        Return output
+    End Function
+
+#End Region
+
+#Region "Button"
+
+    'ボタン0のイベント
+    Private Sub Button0_Click(sender As Object, e As EventArgs) Handles Button0.Click
+        'Err後操作不可にする
+        If errLock Then
+            Return
+        End If
+
+        OperEnqueue("0")
+        Display_Show_Add("0")
+        ButtonEque.Focus()
+    End Sub
+
+    'ボタン1のイベント
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Err後操作不可にする
+        If errLock Then
+            Return
+        End If
+
+        OperEnqueue("1")
+        Display_Show_Add("1")
+        ButtonEque.Focus()
+    End Sub
+
+    'ボタン2のイベント
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'Err後操作不可にする
+        If errLock Then
+            Return
+        End If
+
+        OperEnqueue("2")
+        Display_Show_Add("2")
+        ButtonEque.Focus()
+    End Sub
+
+    'ボタン3のイベント
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        'Err後操作不可にする
+        If errLock Then
+            Return
+        End If
+
+        OperEnqueue("3")
+        Display_Show_Add("3")
+        ButtonEque.Focus()
+    End Sub
+
+    'ボタン4のイベント
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        'Err後操作不可にする
+        If errLock Then
+            Return
+        End If
+
+        OperEnqueue("4")
+        Display_Show_Add("4")
+        ButtonEque.Focus()
+    End Sub
+
+    'ボタン5のイベント
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        'Err後操作不可にする
+        If errLock Then
+            Return
+        End If
+
+        OperEnqueue("5")
+        Display_Show_Add("5")
+        ButtonEque.Focus()
+    End Sub
+
+    'ボタン6のイベント
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        'Err後操作不可にする
+        If errLock Then
+            Return
+        End If
+
+        OperEnqueue("6")
+        Display_Show_Add("6")
+        ButtonEque.Focus()
+    End Sub
+
+    'ボタン7のイベント
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        'Err後操作不可にする
+        If errLock Then
+            Return
+        End If
+
+        OperEnqueue("7")
+        Display_Show_Add("7")
+        ButtonEque.Focus()
+    End Sub
+
+    'ボタン8のイベント
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        'Err後操作不可にする
+        If errLock Then
+            Return
+        End If
+
+        OperEnqueue("8")
+        Display_Show_Add("8")
+        ButtonEque.Focus()
+    End Sub
+
+    'ボタン9のイベント
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        'Err後操作不可にする
+        If errLock Then
+            Return
+        End If
+
+        OperEnqueue("9")
+        Display_Show_Add("9")
+        ButtonEque.Focus()
+    End Sub
+
+    'ボタンCのイベント
+    Private Sub ButtonC_Click(sender As Object, e As EventArgs) Handles ButtonC.Click
+        OperEnqueue("C")
+        Display_Clear()
+        ButtonEque.Focus()
+    End Sub
+
+    'ボタン[.]のイベント
+    Private Sub ButtonDot_Click(sender As Object, e As EventArgs) Handles ButtonDot.Click
+        'Err後操作不可にする
+        If errLock Then
+            Return
+        End If
+
+        OperEnqueue(".")
+        Display_Show_Dot()
+        ButtonEque.Focus()
+    End Sub
+
+    'ボタン[+]のイベント
+    Private Sub ButtonAdd_Click(sender As Object, e As EventArgs) Handles ButtonAdd.Click
+        'Err後操作不可にする
+        If errLock Then
+            Return
+        End If
+
+        OperEnqueue("Oper")
+        CalcModeEnqueue(1)
+        NumEnqueue(LabelDisplay.Text)
+
+        If numQueueList.Count = 2 And calcModeQueueList.Count = 2 And Not operQueueList.Peek = "Oper" And Not operQueueList.Peek = "Eque" Then
+            Process(calcModeQueueList.Peek)
+        End If
+        ButtonEque.Focus()
+    End Sub
+
+    'ボタン[-]のイベント
+    Private Sub ButtonSub_Click(sender As Object, e As EventArgs) Handles ButtonSub.Click
+        'Err後操作不可にする
+        If errLock Then
+            Return
+        End If
+
+        OperEnqueue("Oper")
+        CalcModeEnqueue(2)
+        NumEnqueue(LabelDisplay.Text)
+
+        If numQueueList.Count = 2 And calcModeQueueList.Count = 2 And Not operQueueList.Peek = "Oper" And Not operQueueList.Peek = "Eque" Then
+            Process(calcModeQueueList.Peek)
+        End If
+        ButtonEque.Focus()
+    End Sub
+
+    'ボタン[×]のイベント
+    Private Sub ButtonMulti_Click(sender As Object, e As EventArgs) Handles ButtonMulti.Click
+        'Err後操作不可にする
+        If errLock Then
+            Return
+        End If
+
+        OperEnqueue("Oper")
+        CalcModeEnqueue(3)
+        NumEnqueue(LabelDisplay.Text)
+
+        If numQueueList.Count = 2 And calcModeQueueList.Count = 2 And Not operQueueList.Peek = "Oper" And Not operQueueList.Peek = "Eque" Then
+            Process(calcModeQueueList.Peek)
+        End If
+        ButtonEque.Focus()
+    End Sub
+
+    'ボタン[÷]のイベント
+    Private Sub ButtonDiv_Click(sender As Object, e As EventArgs) Handles ButtonDiv.Click
+        'Err後操作不可にする
+        If errLock Then
+            Return
+        End If
+
+        OperEnqueue("Oper")
+        CalcModeEnqueue(4)
+        NumEnqueue(LabelDisplay.Text)
+
+        If numQueueList.Count = 2 And calcModeQueueList.Count = 2 And Not operQueueList.Peek = "Oper" And Not operQueueList.Peek = "Eque" Then
+            Process(calcModeQueueList.Peek)
+        End If
+        ButtonEque.Focus()
+    End Sub
+
+    'ボタン[=]のイベント
+    Private Sub ButtonEque_Click(sender As Object, e As EventArgs) Handles ButtonEque.Click
+        'Err後操作不可にする
+        If errLock Then
+            Return
+        End If
+
+        OperEnqueue("Eque")
+        NumEnqueue(LabelDisplay.Text)
+        Dim tmpCalcMode As Array = calcModeQueueList.ToArray()
+        If numQueueList.Count = 2 And Not operQueueList.Peek = "Oper" And Not operQueueList.Peek = "Eque" And calcModeQueueList.Count > 0 Then
+            Process(tmpCalcMode(tmpCalcMode.Length - 1))
+            numQueueList.Clear()
+            calcModeQueueList.Clear()
+        End If
+    End Sub
+
+    'ボタン[±]のイベント
+    Private Sub ButtonSymbol_Click(sender As Object, e As EventArgs) Handles ButtonSymbol.Click
+        'Err後操作不可にする
+        If errLock Then
+            Return
+        End If
+
+        Dim display As Double = LabelDisplay.Text
+        display = display * -1
+        LabelDisplay.Text = display
+        ButtonEque.Focus()
+    End Sub
+
+#End Region
+
+#Region "Shortcut"
+
+    'キーボードのショットカット
+    Private Sub Calculator_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        If e.KeyCode = Keys.NumPad1 Then
+            Button1_Click(sender, e)
+        ElseIf e.KeyCode = Keys.NumPad2 Then
+            Button2_Click(sender, e)
+        ElseIf e.KeyCode = Keys.NumPad3 Then
+            Button3_Click(sender, e)
+        ElseIf e.KeyCode = Keys.NumPad4 Then
+            Button4_Click(sender, e)
+        ElseIf e.KeyCode = Keys.NumPad5 Then
+            Button5_Click(sender, e)
+        ElseIf e.KeyCode = Keys.NumPad6 Then
+            Button6_Click(sender, e)
+        ElseIf e.KeyCode = Keys.NumPad7 Then
+            Button7_Click(sender, e)
+        ElseIf e.KeyCode = Keys.NumPad8 Then
+            Button8_Click(sender, e)
+        ElseIf e.KeyCode = Keys.NumPad9 Then
+            Button9_Click(sender, e)
+        ElseIf e.KeyCode = Keys.NumPad0 Then
+            Button0_Click(sender, e)
+        ElseIf e.KeyCode = Keys.Decimal Then
+            ButtonDot_Click(sender, e)
+        ElseIf e.KeyCode = Keys.Delete Or e.KeyCode = Keys.Back Then
+            ButtonC_Click(sender, e)
+        ElseIf e.KeyCode = Keys.Enter Then
+            ButtonEque_Click(sender, e)
+        ElseIf e.KeyCode = Keys.Add Then
+            ButtonAdd_Click(sender, e)
+        ElseIf e.KeyCode = Keys.Subtract Then
+            ButtonSub_Click(sender, e)
+        ElseIf e.KeyCode = Keys.Multiply Then
+            ButtonMulti_Click(sender, e)
+        ElseIf e.KeyCode = Keys.Divide Then
+            ButtonDiv_Click(sender, e)
+        End If
+    End Sub
+
+#End Region
+
+End Class
